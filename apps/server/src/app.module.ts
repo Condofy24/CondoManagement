f0fd188958174ad5871e811f28ad7cc6c489af45
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2024-01-26 13:57:49 -0500

    feat: connect to db

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://capstone:marioisgay@condofy.drswlo1.mongodb.net/?retryWrites=true&w=majority', // TODO: Move password to env
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
