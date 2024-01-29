bb24e13faca6ffea46cc09a9642034aa3b60c45a
Khash <khashayar1924@gmail.com>
2024-01-28 21:32:07 -0500

    Add global header

---
+++ apps/client/app/components/header.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { motion } from "framer-motion";
+import { links } from "@/lib/data";
+import Link from "next/link";
+import clsx from "clsx";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import logo from "@/public/logo.png";
+import Image from "next/image";
+
+export default function Header() {
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
+
+  return (
+    <header className="relative z-[999]">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        className="fixed h-[5rem] w-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:border-black/40 dark:bg-gray-950 sm:h-[3.25rem]"
+      ></motion.div>
+      <div className="absolute left-4 top-2">
+        <Image
+          src={logo}
+          alt="company logo"
+          quality={100}
+          objectFit="cover"
+          height={40}
+          width={40}
+        />
+      </div>
+      <nav className="fixed left-1/2 flex h-8 -translate-x-1/2 py-2 sm:h-[initial]">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="relative flex h-3/4 items-center justify-center"
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              key={link.hash}
+            >
+              <Link
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 transition hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300",
+                  {
+                    "text-gray-950 dark:text-gray-200":
+                      activeSection === link.name,
+                  },
+                )}
+                href={link.hash}
+                onClick={() => {
+                  setTimeOfLastClick(Date.now());
+                  setActiveSection(link.name);
+                }}
+              >
+                {link.name}
+                {link.name === activeSection && (
+                  <motion.span
+                    className="absolute inset-0 -z-10 rounded-full bg-gray-100 dark:bg-gray-800"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
