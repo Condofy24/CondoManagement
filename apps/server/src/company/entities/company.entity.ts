398dcfd010ee5a74775ef604c08ae933d8b198b7
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2024-01-29 09:27:11 -0500

    fix: add index

--- apps/server/src/company/entities/company.entity.ts
+++ apps/server/src/company/entities/company.entity.ts
@@ -4,7 +4,7 @@ export const CompanySchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
     location: { type: String, required: true },
-    companyId: { type: String, required: true, unique: true },
+    companyId: { type: String, required: true, unique: true, index: true },
   },
   {
     timestamps: true, // This enables automatic createdAt and updatedAt fields
