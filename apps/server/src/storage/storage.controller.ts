b3de563da9c83d149a45d0427adb34eef2f6a050
mario-elshaer <mario.elshaer@gmail.com>
2024-02-19 17:08:17 -0500

    feat(property-management): Adding Storage Controller

---
+++ apps/server/src/storage/storage.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateStorageDto } from './dto/create-storage.dto';
+import { StorageService } from './storage.service';
+
+@Controller('storage')
+export class storageController {
+  constructor(private readonly storageService: StorageService) {}
+
+  @Post(':buildingId')
+  create(
+    @Param('buildingId') buildingId: string,
+    @Body() createStorageDto: CreateStorageDto,
+  ) {
+    return this.storageService.createStorage(buildingId, createStorageDto);
+  }
+}
