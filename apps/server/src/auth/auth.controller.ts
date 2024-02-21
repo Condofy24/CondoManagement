0a3df4c13bfd58bea3533dbce543edb58a9b5e3c
Khash <khashayar1924@gmail.com>
2024-02-20 21:18:36 -0500

    Setup mock for mongo and refactor unit tests

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -1,33 +1,15 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  HttpStatus,
-  Post,
-  Get,
-  UseGuards,
-  Res,
-  Req,
-} from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
-import { PrivilegeGuard } from './auth.guard';
-import { Roles } from './decorators/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @HttpCode(HttpStatus.UNAUTHORIZED)
   @Post('login')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
-
-  // @Get('test')
-  // @Roles(2)
-  // @UseGuards(PrivilegeGuard)
-  // test(@Req() req: Request) {
-  //   return this.authService.test();
-  // }
 }
