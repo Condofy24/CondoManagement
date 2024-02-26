d111a8ae6d7e427d4edbc203cfc353cf1aff19a4
yep-pushian <yprempushian@gmail.com>
2024-02-26 17:30:22 -0500

    started user profile

--- apps/client/lib/validation-schemas.ts
+++ apps/client/lib/validation-schemas.ts
@@ -39,3 +39,16 @@ export const loginSchema = object({
 });
 
 export type TLoginSchema = TypeOf<typeof loginSchema>;
+
+
+export const updateUserProfileSchema = object({
+  email: string().email({ message: "A valid email is required" }),
+  name: string()
+    .min(3, { message: "Name must be at least 3 characters long" })
+    .max(20, { message: "Name cannot exceed 20 characters" }),
+  phoneNumber: string()
+    .min(10, { message: "Phone number must be valid" })
+    .max(10, { message: "Phone number must be valid" }), 
+});
+
+export type TUpdateUserProfileSchema = TypeOf<typeof updateUserProfileSchema>;
--- apps/client/redux/services/auth-service.ts
+++ apps/client/redux/services/auth-service.ts
@@ -95,3 +95,49 @@ export const registerManager = createAsyncThunk<void, ManagerRegistrationData>(
     toast.success("Registered successfully");
   },
 );
+
+interface UpdateUserData {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  profilePic: File;
+}
+
+export const updateUserProfile = createAsyncThunk<void, UpdateUserData>(
+  "auth/updateUserProfile",
+  async (updateData, { rejectWithValue }) => {
+    try {
+      const formData = new FormData();
+
+      // Append fields to the form data. If the profile picture is optional, check if it's provided.
+      formData.append('name', updateData.name);
+      formData.append('email', updateData.email);
+      formData.append('phoneNumber', updateData.phoneNumber);
+      if (updateData.profilePic) {
+        formData.append('profilePic', updateData.profilePic);
+      }
+
+  
+
+      // Perform the PUT request to update user profile
+      const response = await axios.put(`${API_URL}/user/profile`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data', 
+         
+        },
+      });
+
+      // Handle the response accordingly
+      toast.success("Profile updated successfully!");
+      return response.data;
+    } catch (error: any) {
+      if (error.response && error.response.data.message) {
+        toast.error(error.response.data.message);
+        return rejectWithValue(error.response.data.message);
+      } else {
+        toast.error("An error occurred while updating the profile.");
+        return rejectWithValue(error.message);
+      }
+    }
+  },
+);
