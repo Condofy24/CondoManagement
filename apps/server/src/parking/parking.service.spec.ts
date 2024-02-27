c6f19ddf2df3b138706f0b6ccaf6954a91678f5a
mario-elshaer <mario.elshaer@gmail.com>
2024-02-27 16:20:50 -0500

    feat(Unit Tests for Parking and Storage): Create parking service spec class

---
+++ apps/server/src/parking/parking.service.spec.ts
@@ -0,0 +1,89 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { UnitService } from '../unit/unit.service';
+import { BuildingService } from '../building/building.service';
+import { ObjectId } from 'mongodb';
+import { Building } from '../building/entities/building.entity';
+import { CreateParkingDto } from './dto/create-parking.dto';
+import { ParkingService } from './parking.service';
+import { Parking, ParkingModel } from './entities/parking.entity';
+import { create } from 'domain';
+import { HttpException } from '@nestjs/common';
+import { Unit } from 'src/unit/entities/unit.entity';
+
+const mockingoose = require('mockingoose')
+
+const createParkingDto: CreateParkingDto = {
+    parkingNumber:4,
+    isOccupied: false,
+    fees:4
+}
+
+const buildingInfoTestData: Building = {   
+    companyId: new ObjectId(), 
+    name:"khaled",
+    address:"aslkdjfalk",
+    unitCount:56,
+    parkingCount:53,
+    storageCount:52,
+    fileUrl:"https://res.cloudinary.com/dzu5t20lr/image/upload/v1708240883/wfypsvm",
+    filePublicId:"wfypsvm4kykgjtxxolbn",
+    fileAssetId:"dc1dc5cbafbe598f40a9c1c8938e51c7"
+}
+
+const occupiedUnitInfoTestData: Unit = {
+    buildingId: new ObjectId(),
+    ownerId: new ObjectId(),
+    renterId: new ObjectId,
+    unitNumber: 5,
+    size: 4.5,
+    isOccupiedByRenter: true,
+    fees: 500
+}
+
+const parkingInfoTestData: Parking ={
+    buildingId: new ObjectId(),
+    parkingNumber: 40,
+    isOccupied: false,
+    fees: 12.50
+}
+
+const buildingServiceMock = {
+    findOne: jest.fn().mockResolvedValue(buildingInfoTestData),
+    findByIdandUpdateParkingCount: jest.fn().mockResolvedValue(null),
+}
+
+const unitServiceMock = {
+    findOne: jest.fn().mockResolvedValue(occupiedUnitInfoTestData)
+}
+
+describe('ParkingService', () => {
+    let service: ParkingService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers:[
+                ParkingService,
+                {
+                    provide: getModelToken('Parking'),
+                    useValue: ParkingModel
+
+                },
+                {
+                    provide: BuildingService,
+                    useValue:buildingServiceMock
+                },
+                {
+                    provide: UnitService,
+                    useValue: unitServiceMock
+                },
+            ]
+        }).compile();
+        service = module.get<ParkingService>(ParkingService);
+    });
+
+    afterEach(() => {
+        mockingoose(ParkingModel).reset();
+        jest.clearAllMocks();
+    });
+});
