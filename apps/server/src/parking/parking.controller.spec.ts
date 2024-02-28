92b51b3cea85e21d6623924d5fdc99a82b89abd1
mario-elshaer <mario.elshaer@gmail.com>
2024-02-28 13:56:17 -0500

    feat(Property-Mnagement): Cleaning code in parking controller spec

--- apps/server/src/parking/parking.controller.spec.ts
+++ apps/server/src/parking/parking.controller.spec.ts
@@ -1,14 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ObjectId } from 'mongodb';
-import { User } from '../user/entities/user.entity';
-import { Building } from '../building/entities/building.entity';
 import { ParkingController } from './parking.controller';
 import { CreateParkingDto } from './dto/create-parking.dto';
 import { LinkParkingToUnitDto } from './dto/link-parking-to-unit.dtp';
 import { Parking } from './entities/parking.entity';
 import { ParkingService } from './parking.service';
 import { HttpStatus } from '@nestjs/common';
-import { Unit } from 'src/unit/entities/unit.entity';
 
 const createParkingDto: CreateParkingDto = {
   parkingNumber: 4,
@@ -25,19 +22,6 @@ const parkingServiceMock = {
 
 const linkParkingToUnitDto: LinkParkingToUnitDto = {
   parkingNumber: 4,
-};
-
-const buildingInfoTestData: Building = {
-  companyId: new ObjectId(),
-  name: 'khaled',
-  address: 'aslkdjfalk',
-  unitCount: 56,
-  parkingCount: 53,
-  storageCount: 52,
-  fileUrl:
-    'https://res.cloudinary.com/dzu5t20lr/image/upload/v1708240883/wfypsvm',
-  filePublicId: 'wfypsvm4kykgjtxxolbn',
-  fileAssetId: 'dc1dc5cbafbe598f40a9c1c8938e51c7',
 };
 
 const buildingInfoTestData2 = {
@@ -67,16 +51,6 @@ const parkingInfoTestData2 = {
   parkingNumber: 4,
   isOccupied: false,
   fees: 4,
-};
-
-const occupiedUnitInfoTestData: Unit = {
-  buildingId: buildingInfoTestData2.id,
-  ownerId: new ObjectId(),
-  renterId: new ObjectId(),
-  unitNumber: 5,
-  size: 4.5,
-  isOccupiedByRenter: true,
-  fees: 500,
 };
 
 describe('ParkingController', () => {
