d286ed7718234064815618a34aa2e11f0c5470ce
mario-elshaer <mario.elshaer@gmail.com>
2024-03-24 21:29:07 -0400

    feat(requests): add request model

---
+++ apps/server/src/requests/models/request.model.ts
@@ -0,0 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  RequestEntity,
+  RequestStatus,
+  RequestType,
+} from '../entities/request.entity';
+
+export class RequestModel {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty({ enum: RequestType })
+  type: RequestType;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty({ enum: RequestStatus })
+  status: RequestStatus;
+
+  @ApiProperty({ type: 'string', format: 'date-time' })
+  resolutionTime?: Date;
+
+  @ApiProperty()
+  resolutionContent?: string;
+
+  @ApiProperty()
+  owner: string; 
+
+  constructor(entity: RequestEntity) {
+    this.id = entity._id.toString();
+    this.type = entity.type;
+    this.title = entity.title;
+    this.description = entity.description;
+    this.status = entity.status;
+    this.resolutionTime = entity.resolutionTime;
+    this.resolutionContent = entity.resolutionContent;
+    this.owner = entity.owner.toString(); 
+  }
+}
