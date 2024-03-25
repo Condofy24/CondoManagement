5cbcdfb9b355882f8587de2ee4f03a21e51f14ff
mario-elshaer <mario.elshaer@gmail.com>
2024-03-25 18:41:26 -0400

    feat(requests): Add create method for service

---
+++ apps/server/src/requests/request.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateRequestDto } from './dto/create-request.dto';
+import { UpdateRequestDto } from './dto/update-request.dto';
+import { RequestEntity } from './entities/request.entity';
+
+@Injectable()
+export class RequestService {
+  constructor(
+    @InjectModel('Request') private readonly requestModel: Model<RequestEntity>,
+  ) {}
+
+  /**
+   * Creates a new request.
+   *
+   * @param ownerId - The ID of the owner submitting the request.
+   * @param createRequestDto - The data transfer object for request creation.
+   * @returns A promise resolved with the created request entity.
+   */
+  async create(
+    ownerId: string,
+    createRequestDto: CreateRequestDto,
+  ): Promise<RequestEntity> {
+    const createdRequest = new this.requestModel({
+      ...createRequestDto,
+      owner: ownerId,
+    });
+    return createdRequest.save();
+  }
+}
